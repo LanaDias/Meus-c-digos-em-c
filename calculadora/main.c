b0779f9705c5b77b53df133e006d1f7c2fbad8b5
LanaDias <169058780+LanaDias@users.noreply.github.com>
2024-05-15 00:17:58 -0300

    Add files via upload

---
+++ calculadora/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // criar calculadora que so calcula números positivos
+
+    float n1, n2, re;
+    char operador;
+
+    //dados do user
+    printf("Digite dois numeros para serem calculados:\n");
+    scanf("%f %f", &n1, &n2);
+// operador
+    printf("Digite um operador (+, -, *,/):\n");
+    scanf(" %c", &operador);
+
+    //tratamento de erro
+    if (n1 <0 || n2< 0)
+    {
+        printf("INVALIDO");
+    }
+
+    else
+    {
+        switch (operador)
+        {
+        case '+':
+            re = n1 + n2 ;
+            printf("Resultado da soma: %.2f\n", re);
+            break;
+
+        case '-':
+            re = n1 - n2;
+            printf("subtração: %.2f", re);
+            break;
+        case '*':
+            re = n1 * n2;
+            printf("multiplicação: %.2f", re);
+            break;
+        case '/':
+            if (n2 !=0)
+            {
+                re = n1 / n2;
+                printf("divisão: %.2f", re);
+            }
+            else
+            {
+                printf("Erro: operador inválido.\n");
+            }
+            break;
+        }
+
+    }
+    return 0;
+}
